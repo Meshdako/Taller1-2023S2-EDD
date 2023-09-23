54f03f88a3bc7b5ea7f338ce54ca47a5cc66e241
Meshdako <msobarzo@utem.cl>
2023-09-23 03:16:48 -0300

    Clase fecha creada

---
+++ Class/fecha.cpp
@@ -0,0 +1,27 @@
+#include "fecha.h"
+
+Fecha::Fecha()
+{
+    this->dia = 0;
+    this->mes = 0;
+    this->anio = 0;
+}
+
+Fecha::Fecha(int dia, int mes, int anio)
+{
+    this->dia = dia;
+    this->mes = mes;
+    this->anio = anio;
+}
+
+Fecha::~Fecha()
+{
+    // Empty
+}
+
+void Fecha::verFecha() const
+{
+    std::cout << "Día: " << dia << std::endl;
+    std::cout << "Mes: " << mes << std::endl;
+    std::cout << "Año: " << anio << std::endl;
+}
