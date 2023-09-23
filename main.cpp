a9d9255789b381f406cfec27e6fd863fb755c851
Meshdako <msobarzo@utem.cl>
2023-09-23 03:19:52 -0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+// Clases
+#include "Class/rut.h"
+#include "Class/rut.cpp"
+#include "Class/fecha.h"
+#include "Class/fecha.cpp"
+#include "Class/direccion.h"
+#include "Class/direccion.cpp"
+#include "Class/patente.h"
+#include "Class/patente.cpp"
+
+#define Maximum_Digits 10
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
     return 0;
 }
