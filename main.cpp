be8d12eee5359f597664efae891f1c2ab8e81598
Meshdako <msobarzo@utem.cl>
2023-10-03 02:40:04 -0300

    Agregado Menú de opciones al archivo main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 
+#include "funciones.hpp"
+#include "funciones.cpp"
+
 // Clases
-#include "Class/rut.h"
+#include "Class/rut.hpp"
 #include "Class/rut.cpp"
-#include "Class/fecha.h"
+#include "Class/fecha.hpp"
 #include "Class/fecha.cpp"
-#include "Class/direccion.h"
+#include "Class/direccion.hpp"
 #include "Class/direccion.cpp"
-#include "Class/patente.h"
+#include "Class/patente.hpp"
 #include "Class/patente.cpp"
 
 #define Maximum_Digits 10
+#define PATENTES 800
 
 int main() {
+    Patente p[PATENTES];
 
+    int indice = 0;
+
+    std::string datos = "patentesComerciales.txt";
+
+    leerDatos(datos, p, indice);
+
+    Menu(p, indice);
 
     return 0;
 }
